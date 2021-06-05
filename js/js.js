14dbd265fb33b307a4fcc749f8f3fa18038fe40d
unknown <d.seegers.mail@gmail.com>
2021-06-05 14:27:31 +0200

    add some javascript

--- js/js.js
+++ js/js.js
@@ -2,8 +2,12 @@ $(function() {
     $("#click").on("click", function () {
         let value = $("#names").val();
         let newValue = value.split(",");
+
         let itemId = (Math.floor(Math.random() * (newValue.length)));
         $("#winner").html(newValue[itemId]);
         $(".winner").show();
+        if(typeof newValue[1] === 'undefined'){
+            $("#winner").html("Wellicht handig om meerdere kandidaten in te vullen")
+        }
     });
 });
